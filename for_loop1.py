5fc821e17d16a8d51d4ade5883ee11a8d5c7bdaa
oldtree61 <daozei61@126.com>
2019-02-11 12:09:14 +0800

    用于GIT练习

---
+++ for_loop1.py
@@ -0,0 +1,15 @@
+my_list = ["jake", "Tom", "xi li"]
+
+# for name in my_list:
+#     print(name)
+
+# for number in range(11):
+#     print(number)
+
+
+for letter in 'inty':
+    print("each letter is: " + letter)
+
+'''这是github的demo'''
+'''inty的'''
+'''这样可否'''
